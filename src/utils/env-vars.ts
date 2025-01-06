f4320a7238dc31d0248dadf1d90c32c6f5c53a49
ahmedRAOUANE <ahmedraouane30@gmail.com>
2025-01-06 11:57:15 +0100

    build the authentication system

--- src/utils/env-vars.ts
+++ src/utils/env-vars.ts
@@ -1,2 +1,2 @@
-export const supabaseUrl = process.env.SUPABASE_URL as string;
-export const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string;
+export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+export const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
